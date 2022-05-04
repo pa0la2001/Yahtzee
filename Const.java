73905ab5e885e7c733aa18cf0e9bc600169cb472
pa0la2001 <102731647+pa0la2001@users.noreply.github.com>
2022-05-04 09:22:46 +0200

    Add files via upload

---
+++ Const.java
@@ -0,0 +1,7 @@
+package com.grupi2c.yahtzee.constants;
+
+public class Const {
+    public static final String GAME_NAME="Yahtzee";
+    public static final String DICE="Zari";
+    public static final String WELCOME_MESSAGE="Mireseerdhet ne Yahtzee!";
+}
